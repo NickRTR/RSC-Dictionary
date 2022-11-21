12fb6b099fee6981e9b66900911fadfb64ee40d5
NickRTR <95316598+NickRTR@users.noreply.github.com>
2022-11-21 16:58:05 +0100

    update website description

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,7 +7,7 @@
 <body>
 	<h1>RSC Dictionary</h1>
 
-	<p>Find the right Response Status Codes for your type of Response.</p>
+	<p>HTTP Response Status Code Dictionary</p>
 
 	<form method="POST" use:enhance>
 		<input type="number" name="code" placeholder="Code" />
