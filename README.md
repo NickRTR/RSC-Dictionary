151aad1659732af451ad5d9288fe2ba517693d6a
NickRTR <95316598+NickRTR@users.noreply.github.com>
2022-11-21 12:32:39 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# RSC Dictionary
+
+Find the right Response Status Codes for your type of Response.
---
+++ src/routes/+page.svelte
@@ -0,0 +1,3 @@
+<h1>RSC Dictionary</h1>
+
+<p>Find the right Response Status Codes for your type of Response.</p>
